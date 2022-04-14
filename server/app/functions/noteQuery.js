8b822726210a6c32788f493a8f184e0ec0873a5f
Timothy197 <1195796206@qq.com>
2022-04-14 09:07:03 +0800

    minor update

--- server/app/functions/noteQuery.js
+++ server/app/functions/noteQuery.js
@@ -18,7 +18,7 @@ async function getLocalFolderNote(userId) {
     const foldersFound = await Folder.find(folderFilter).sort({title: 1});
     //console.log(foldersFound);
     for (var i=0; i<foldersFound.length; i++){
-        var notesInFolder = foldersFound[i];
+        var notesInFolder = foldersFound[i].notes;
         //console.log(notesInFolder);
         var temp = {
             folder: foldersFound[i]._id,
