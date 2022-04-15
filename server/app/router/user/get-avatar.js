d030e28b769ec2eda29de97dc2a6f4a69b2f8f4e
Gary Chen <chenhhgary@gmail.com>
2022-04-16 00:23:28 +0800

    update get-avatar API

--- server/app/router/user/get-avatar.js
+++ server/app/router/user/get-avatar.js
@@ -1,7 +1,7 @@
 const User = require("../../model/User");
 
 module.exports = async (req, res) => {
-    const query = User.findOne({_id: req.body._id});
+    const query = User.findOne({_id: req.query.uid});
     query.select('_id avatar');
     query.exec((err, user)=>{
         if (err) { return res.status(404).send({message: 'user with _id not found'}); }
