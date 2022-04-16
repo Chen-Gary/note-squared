d52b12baf55d199fc84ab876348fb27d25c9afe4
Frida161 <65965736+Frida161@users.noreply.github.com>
2022-04-16 16:47:19 +0800

    add search in note community

--- front/src/components/note_community/Community.vue
+++ front/src/components/note_community/Community.vue
@@ -9,6 +9,9 @@
         <el-input v-model="keyword" placeholder="Enjoy your journey ~" style="width: 30%"
         @keydown.enter.native = "searchEnterFun"></el-input>
      </span>
+     <span>
+      <el-button class="note_refresh" type="text" @click="refreshNote"><i class="el-icon-refresh"></i></el-button>
+     </span>
   </div>
 
   <div>
@@ -18,8 +21,8 @@
   <el-pagination
     @size-change="handleSizeChange"
     @current-change="handleCurrentChange"
-    :current-page="currentPage"
-    :total="20"
+    :current-page="this.page.currentPage"
+    :total="this.page.totalPage"
     layout="prev, pager, next"
     >
    </el-pagination>
@@ -40,6 +43,7 @@ export default {
   },
   created()
   {
+    //更新数据库的notes列表
      let obj = {title:"note1", description:"it's a new note", thumb:"0",username:"Frida",url:"www.hello.com" }
      this.notes_info.note_obj.list.push(obj);
      this.notes_info.note_obj.list.push({title:"note2", description:"it's a new note", thumb:"0",username:"Frida",url:"www.hello.com" });
@@ -58,28 +62,51 @@ export default {
             currentPage:1, //当前页
             pagesize:10,    // 每页的数据
             rows:1,         //总条数
-            totalPage:5, //总页数
+            totalPage:5,  //总条数
       },
+      mode:"search", //当前搜索模式search，当前所有数据库模式all
     }
   },
   methods: {
     searchEnterFun(e)
     {
+      this.mode = "search";//搜索模式
+      this.page.currentPage = 1;//调整页面为搜索的第一页
       var keycode = window.event? e.keyCode:e.which;
       if (keycode == 13){
               console.log("press enter");
         this.search(); //回车键触发  搜索事件
       }
-      console.log(press);
     },
 
-    //搜索，等待后端API
+    //搜索
     search(){
-      //要删除的
-      this.notes_info.note_obj.list = [{title:"search", description:"it's a new note", thumb:"0",username:"Frida",url:"www.hello.com" }];
+      this.$axios.get("/note/search-public?search="+this.keyword+"&page="+this.page.currentPage,{
+        })
+          .then(response=>{
+            console.log("status:")
+            console.log(response.data)
+            let note_list = {}
+            if(response.status === 200){
+              this.tableData = []
+              this.page.totalPage = response.data.pagesize * response.data.total;
+              for (let i=0; i<response.data.notes.length; i++){
+                let obj = response.data.notes[i];
+                note_list[i] =  {title:obj.title, description:obj.description, thumb:obj.like,username:obj.publishDate.slice(0,10),url:obj._id };
+              }
+               this.notes_info.note_obj.list = note_list;
+            }
+            else {
+              alert("Incorrect search operation")
+            }
+          })
+          .catch(function (error) {
+            alert("Incorrect search operation")
+            console.log(error)
+          })
+    
     },
-
-
+     
     //https://www.bilibili.com/video/BV1hA41187dE?spm_id_from=333.337.search-card.all.click   47:21
 
     // 初始页currentPage、初始每页数据数pagesize和数据data
@@ -90,14 +117,27 @@ export default {
     
     handleCurrentChange: function(currentPage){
         this.page.currentPage = currentPage;
-        this.handleUserList();
+        this.UpdatePage();
         console.log(this.page.currentPage)  //点击第几页
     },
     
-    handleUserList() {    
-      //等待后端完善所有每个页面
-        this.notes_info.note_obj.list = [{title:"change page", description:"it's a new note", thumb:"0",username:"Frida",url:"www.hello.com" }];
+    UpdatePage() {    
+      if (this.mode == "search"){
+        this.search();
+      }
+      //等待后端完善所有数据库的返回
+      else{
+
+      }
         
+    },
+    //重新返回最开始所有数据库的页面，清空搜索栏
+    refreshNote(){
+      //清空搜索栏
+      this.keyword = "";
+      //清空回到数据库的第一页
+      this.page.currentPage = 1;
+      //等待后端完善所有数据库的返回
     }
 
   },
@@ -112,5 +152,9 @@ export default {
 .search_layout{
   margin-top:3%;
   margin-bottom: 3%;
+}
+.note_refresh{
+  font-size: 25px;
+  margin-left:10px;
 }
 </style>
